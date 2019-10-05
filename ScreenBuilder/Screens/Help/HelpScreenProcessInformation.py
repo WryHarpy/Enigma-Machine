f9a63893de396ff4cce78c28ec6963b2eee73370
WryHarpy <54758104+WryHarpy@users.noreply.github.com>
2019-10-05 15:30:24 -0400

    More help screen screens.

---
+++ ScreenBuilder/Screens/Help/HelpScreenProcessInformation.py
@@ -0,0 +1,34 @@
+'''
+Program: HelpScreenProcessInformation.py
+Author : John Hayes
+Date    : Oct, 4 2019
+Synopis: This is the process information screen.
+'''
+def PrcInf():
+    print("CIS220 Project:TheEnigmaMachine              Programmers: CIS220 Class Fall 2019")
+    print("")
+    print("")
+    print("")
+    print("				  Enigma Machine")
+    print("")
+    print("")
+    print("		               Process Information")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    print("")
+    sel = input("Please enter a number.")
+    return sel
+PrcInf()
+input()
