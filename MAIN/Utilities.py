fea728ffa6e07bb561acbc07274011e96e1e65e0
ONE-i <54591672+ONE-i@users.noreply.github.com>
2019-10-28 09:46:22 -0400

    Add files via upload

---
+++ MAIN/Utilities.py
@@ -0,0 +1,71 @@
+# Utilities.py
+# Quang Hoang
+# 21 OCT 2019
+
+# Some handy tools
+
+# Open the letter set file and get the letter sets
+def getSet(charSet):
+    doc = open(charSet, 'r')
+    set = doc.read()
+    return set
+
+
+# Re- write the message to file and screen
+def writeMessage():
+    file = open("encrypt message.txt", 'r')
+    file = file.read()
+    message = open("encrypt message.txt", 'w')
+
+    for i in range(0, len(file), 17):
+        message.write(str(file[i:i + 17]) + "  ")
+        print(file[i:i + 17], end='   ')
+
+
+# Get the message
+def getText():
+    # text = input(str("Enter your message: "))
+    replaceTextItems = [' ', '\n', '—', '-']
+    # '''
+    with open("GettysburgAddress.txt", 'r') as text:
+        text = text.read()
+        print(text)
+    # '''
+    for i in replaceTextItems:
+        text = text.replace(i, '')
+    return text
+
+# Write message into a text file doc.txt. This write lines to fit the screen
+def writeDoc(message):
+    #message = open("message.txt", 'r')
+    #message = message.read()
+    doc = open("doc.txt", 'w')
+    for i in range(0, len(message), 60):
+        doc.write("       " + message[i:i + 15] + "  " + message[i + 15:i + 30] + "  " + message[i + 30:i + 45] + "  " + message[i + 45:i + 60] + "       \n")
+    doc.close()
+
+# Get the text from doc.txt
+def getDoc():
+    f = open("doc.txt", 'r')
+    doc = f.readlines()
+    f.close()
+    return doc
+
+# Put the message into an array
+def getMessage(message):
+    doc = []
+    for i in range(0, len(message), 60):
+        doc.append("       " + message[i:i + 15] + "  " + message[i + 15:i + 30] + "  " + message[i + 30:i + 45] + "  " + message[i + 45:i + 60] + "       \n")
+    return doc
+
+'''
+def display(doc):
+    if len(doc) < 13:
+        message(doc)
+    if len(doc) > 13:
+        for i in range(0, len(doc), 13):
+            doc = doc[i:i+13]
+            message(doc)
+'''
+
+
