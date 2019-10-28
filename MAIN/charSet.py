1d28cc90f0aeaf224a2d4f01195ab4993a66c1b2
ONE-i <54591672+ONE-i@users.noreply.github.com>
2019-10-28 09:45:36 -0400

    Create charSet.py

---
+++ MAIN/charSet.py
@@ -0,0 +1,17 @@
+# charSet.py
+# Quang Hoang
+# 21 OCT 2019
+
+# This is the rotors and plugboard
+
+import Utilities
+
+charSet = {
+    "letterSet" : Utilities.getSet("letterSet.txt"),
+    "plugBoard" : Utilities.getSet("plugBoard.txt"),
+    "Rotor_1" : Utilities.getSet("Rotor_1.txt"),
+    "Rotor_2": Utilities.getSet("Rotor_2.txt"),
+    "Rotor_3": Utilities.getSet("Rotor_3.txt"),
+    "Rotor_4": Utilities.getSet("Rotor_4.txt")
+
+}
