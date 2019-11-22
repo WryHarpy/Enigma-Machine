ee45a39b6214037b8ad499ba5dbee19fd4d5cf35
ONE-i <chonhungdieunhamnhi@gmail.com>
2019-11-21 21:16:30 -0500

    Organize

--- evWheels.py
+++
@@ -1,3 +0,0 @@
-Wheels = {"W1":"WBazryRKdHxk.PYmbI tvFeMfZgGEiOQ-qhuDwA,jJcSlLpXCnsTVUoN",
-"W2":"jL,p ZAoDvN-WzrTyYEMnfbicXaQkFHGxhOwIsRKSPeBUtdgq.VulmCJ",
-"W3":" xOhn.yYXVIcotjPAgraMSukeCDdH-B,NpGsqTziFLUfWwQElmZKbvJR",}
