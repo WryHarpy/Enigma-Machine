a29507d2421ea01514271ff8f16aa31224f88813
WryHarpy <54758104+WryHarpy@users.noreply.github.com>
2019-11-25 09:42:32 -0500

    Update PrimeWheelPicker.py

--- PrimeWheelPicker.py
+++ PrimeWheelPicker.py
@@ -1,26 +1,24 @@
-import evWheels
-#importing the wheels
-arr = [] 
-with open("scrambledPrimeNumbers.txt") as file:
-    for line in file: 
-         line = int(line)
-         arr.append(line) 
-#Setting up the array, could just as easily import another dictionary
+import WheelDictionary, primeNumbers
+#importing the wheels and prime numbers
 def primePicker(index):
-    Pointer=(arr[index])
-    while Pointer > 50:
-        Pointer = Pointer - 50
+    Pointer=(primeNumbers.primeNumbers.get(index))
+    while Pointer > 100:
+        Pointer = Pointer - 100
         continue
     wheel = str(Pointer)
-    print (evWheels.Wheels.get('w'+wheel))
+    temp = (WheelDictionary.Wheels.get('w'+wheel))
+    return temp    
 #Function that takes input for index in the prime number list and takes remainder
-print('Enter first prime number list index')
-index1 = int(input(''))
-print('Enter second prime number list index')
-index2 = int(input(''))
-print('Enter third prime number list index')
-index3 = int(input(''))
-primePicker(index1)
-primePicker(index2)
-primePicker(index3)
+def wheelPicker():
+     print('Enter first prime number list index')
+     index1 = int(input(''))
+     print('Enter second prime number list index')
+     index2 = int(input(''))
+     print('Enter third prime number list index')
+     index3 = int(input(''))
+     w1 = primePicker(index1)
+     w2 = primePicker(index2)
+     w3 = primePicker(index3)
+     return w1, w2, w3
 #Driver
+wheelPicker()
